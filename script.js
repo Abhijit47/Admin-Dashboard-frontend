9decfd00f9f3fe3f8b28dead09edb87931506ccf
Abhijit <akarmakar846@gmail.com>
2023-07-22 10:01:03 +0530

    Added few more media queries

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
 'use strict';
-
+const mainContainer = document.querySelector('.main_container');
 const sideNav = document.querySelector('.sidenav');
 const dashboard = document.querySelector('.right_section');
 const menuBtn = document.querySelector('.btn');
 
+const Appbar = document.querySelector('.topbar_wrapper');
+
+function closeMenu(e) {
+  console.log(e);
+  sideNav.classList.remove("sidebar_deactive");
+}
+
+function observerCb(payload) {
+  console.log(payload[0]
+  );
+  // console.log(payload[0].isIntersecting);
+  // if (payload[0].isIntersecting === false) {
+  //   // closeMenu();
+  //   handleMenu();
+  //   observer.unobserve(Appbar);
+  // } else {
+  //   // handleMenu();
+  //   return;
+  // }
+}
+const observer = new IntersectionObserver(observerCb, {
+  root: Appbar,
+  threshold: 1,
+});
+observer.observe(Appbar);
+console.log(observer);
+
+
 // create a function for show/hide menu
+// const handleMenu = (e) => {
+//   // console.dir(e.target);
+//   // console.log("clicked");
+//   sideNav.classList.toggle("sidebar_deactive");
+//   // sideNav.style.display = "none";
+//   dashboard.style.width = "100vw";
+
+// };
+
+// version:2
 const handleMenu = (e) => {
   // console.dir(e.target);
-  // console.log("clicked");
-  sideNav.classList.toggle("sidebar_deactive");
+  console.log("clicked");
+  sideNav.classList.toggle("sidebar_active");
   // sideNav.style.display = "none";
-  dashboard.style.width = "100vw";
-};
 
+};
 
 menuBtn.addEventListener('click', handleMenu);
